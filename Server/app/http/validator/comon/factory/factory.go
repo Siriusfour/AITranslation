cbaa2dee1902c44474ee40ca27066a5a4650e952
sirius <2605116008@qq.com>
2025-10-15 12:02:45 +0800

    10.15

--- Server/app/http/validator/comon/factory/factory.go
+++ Server/app/http/validator/comon/factory/factory.go
@@ -2,21 +2,21 @@ package factory
 
 import (
 	"AITranslatio/Global"
-	"AITranslatio/Global/CustomErrors"
+	"AITranslatio/Global/MyErrors"
 	"AITranslatio/app/core/container"
 	"AITranslatio/app/http/validator/interf"
 	"github.com/gin-gonic/gin"
 )
 
-// 表单参数验证器工厂 （请勿修改）
+// 表单参数验证器工厂
 func Create(key string) func(context *gin.Context) {
-
 	if value := container.CreateContainersFactory().Get(key); value != nil {
 		if val, isOk := value.(interf.ValidatorInterface); isOk {
 			return val.CheckParams
 		}
 	}
 
-	Global.Logger.Error(CustomErrors.ErrorsValidatorNotExists + ", 验证器模块：" + key)
+	Global.Logger.Error(MyErrors.ErrorsValidatorNotExists + ", 验证器模块：" + key)
 	return nil
+
 }
---
+++ Server/app/http/validator/validators/Team/Join.go
@@ -0,0 +1,35 @@
+package Team
+
+import (
+	"AITranslatio/Global/Consts"
+	"AITranslatio/app/http/Controller/ApiController"
+	"AITranslatio/app/http/reposen"
+	"AITranslatio/app/http/validator/comon/data_transfer"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type JoinDTO struct {
+	FromUserID   int64
+	TeamID       int
+	Introduction string
+}
+
+func (DTO *JoinDTO) CheckParams(context *gin.Context) {
+
+	//1.基础绑定验证
+	if err := context.ShouldBindJSON(&DTO); err != nil {
+		reposen.ErrorParam(context, fmt.Errorf("参数基础绑定验证", err))
+	}
+
+	//2.在ctx里添加k=consts.ValidatorPrefix+ json标签对应的 键 => 值 形式绑定在上下文， 再传递给controller
+	extraAddBindDataContext := data_transfer.DataAddContext(DTO, Consts.ValidatorPrefix, context)
+	if extraAddBindDataContext == nil {
+		reposen.ErrorSystem(context, errors.New("DataAddContext无法绑定到*gin.contex"))
+	} else {
+		//调用Controller
+		(&ApiController.ApiController{}).CreateTeam(extraAddBindDataContext)
+	}
+
+}
